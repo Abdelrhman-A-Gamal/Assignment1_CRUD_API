2e614f42dbd4af1398b7d7adb66ef4e42499cb00
Jimmy-307 <abderhmanahmed43@gmail.com>
2026-07-25 19:36:13 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port =3000;
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const swaggerDocument = require('./swagger.json');
 
-/*app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
-});*/
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
